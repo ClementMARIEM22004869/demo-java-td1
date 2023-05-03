d5f350b0416c840299bd2ae027736c5a1ea9004c
ClementMARIEM22004869 <clement.marie24@gmail.com>
2023-05-03 12:14:14 +0200

    update

--- src/main/java/com/exo/exo1/borderPane.java
+++ src/main/java/com/exo/exo1/borderPane.java
@@ -51,10 +51,44 @@ public class borderPane extends Application{
 
         Label label1 = new Label("Name:");
         TextField texte = new TextField();
-        hb.getChildren().addAll(label1, texte);
-        hb.setSpacing(10);
+        borderPane.getChildren().addAll(label1, texte);
 
-        borderPane.setLeft(Bouton1, Bouton2, Bouton3);
+        borderPane.setLeft(Bouton1);
+        borderPane.setLeft(Bouton2);
+        borderPane.setLeft(Bouton3);
+
+        //Creating a GridPane container
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(5);
+        grid.setHgap(5);
+//Defining the Name text field
+        final TextField name = new TextField();
+        name.setPromptText("Enter your first name.");
+        name.setPrefColumnCount(10);
+        name.getText();
+        GridPane.setConstraints(name, 0, 0);
+        grid.getChildren().add(name);
+//Defining the Last Name text field
+        final TextField lastName = new TextField();
+        lastName.setPromptText("Enter your last name.");
+        GridPane.setConstraints(lastName, 0, 1);
+        grid.getChildren().add(lastName);
+//Defining the Comment text field
+        final TextField comment = new TextField();
+        comment.setPrefColumnCount(15);
+        comment.setPromptText("Enter your comment.");
+        GridPane.setConstraints(comment, 0, 2);
+        grid.getChildren().add(comment);
+//Defining the Submit button
+        Button submit = new Button("Submit");
+        GridPane.setConstraints(submit, 1, 0);
+        grid.getChildren().add(submit);
+//Defining the Clear button
+        Button clear = new Button("Clear");
+        GridPane.setConstraints(clear, 1, 1);
+        grid.getChildren().add(clear);
+
 
         // Ajout du conteneur à la scene
         Scene scene = new Scene(borderPane);
